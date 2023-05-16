611c056286331d374eed01070072177d5b3e4b43
Liaxo <57733867+liaxoo@users.noreply.github.com>
2023-05-16 17:52:44 -0400

    colors

--- src/index.js
+++ src/index.js
@@ -8,25 +8,7 @@ import { Notifications } from "@mantine/notifications";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <MantineProvider
-    theme={{
-      colors: {
-        "ocean-blue": ["#FFFFF"],
-        "bright-pink": [
-          "#F0BBDD",
-          "#ED9BCF",
-          "#EC7CC3",
-          "#ED5DB8",
-          "#F13EAF",
-          "#F71FA7",
-          "#FF00A1",
-          "#E00890",
-          "#C50E82",
-          "#AD1374",
-        ],
-      },
-    }}
-  >
+  <MantineProvider>
     <Notifications />
     <App />
   </MantineProvider>
